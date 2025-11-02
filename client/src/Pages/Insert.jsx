9901b408e366e8804a9f4ce29c839385b9279c04
prashant patel <prashantpatel487330@gmail.com>
2025-11-02 23:53:56 +0530

    crud

--- client/src/Pages/Insert.jsx
+++ client/src/Pages/Insert.jsx
@@ -2,6 +2,16 @@ const Insert=()=>{
     return(
         <>
         <h1>Welcome to Insert page</h1>
+        <h1> Insert Data</h1>
+          Enter Empno : <input type="text" name="empno" onChange={handleInput} />
+          <br/>
+            Enter Name : <input type="text" name="name" onChange={handleInput} />
+          <br/>
+            Enter Designation : <input type="text" name="designation" onChange={handleInput} />
+          <br/>
+            Enter Salary : <input type="text" name="salary" onChange={handleInput} />
+          <br/>
+          <button onClick={handleSubmit}>Save!!!</button>
         </>
     )
 }
