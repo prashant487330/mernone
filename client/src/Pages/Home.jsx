cb8bbd84b26af20b3c8116dabc5c85a20c76996e
prashant patel <prashantpatel487330@gmail.com>
2026-02-17 23:49:58 +0530

    jwt

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,27 +1,27 @@
-// import axios from "axios";
-// import { useState, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// const Home = () => {
-//     const navigate = useNavigate();
-//     const validateAuth = async () => {
-//         const token = localStorage.getItem("token");
-//         if (token) {
-//             let api = "http://localhost:8000/employees/userauth";
-//             const response = await axios.post(api, null, { headers: { "auth-token": token } });
-//             console.log(response.data);
-//             localStorage.setItem("username", response.data.username);
-//             localStorage.setItem("email", response.data.email);
-//             navigate("/dashboard");
-//         }
-//     }
-//     useEffect(() => {
-//         validateAuth();
-//     }, [])
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+const Home = () => {
+    const navigate = useNavigate();
+    const validateAuth = async () => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            let api = "http://localhost:8000/employees/userauth";
+            const response = await axios.post(api, null, { headers: { "auth-token": token } });
+            console.log(response.data);
+            localStorage.setItem("username", response.data.username);
+            localStorage.setItem("email", response.data.email);
+            navigate("/dashboard");
+        }
+    }
+    useEffect(() => {
+        validateAuth();
+    }, [])
 
-//     return (
-//         <>
-//             <h1> Welcome To  JWT Login</h1>
-//         </>
-//     )
-// }
-// export default Home;
+    return (
+        <>
+            <h1> Welcome To  JWT Login</h1>
+        </>
+    )
+}
+export default Home;
